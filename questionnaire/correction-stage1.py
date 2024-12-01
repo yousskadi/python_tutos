9f4937ac5cd4503dd6f903224251c89b2291fb31
youssef <youssef.kadi@gmail.com>
2024-12-01 17:07:01 +0000

    questionnaire V2

--- questionnaire/correction-stage1.py
+++ questionnaire/correction-stage1.py
@@ -1,28 +1,62 @@
-def poser_question(question, r1, r2, r3, r4, choix_bonne_reponse):
-    global score
-    print("Question: ")
-    print(" " + question)
-    print(" a. " + r1)
-    print(" b. " + r2)
-    print(" c. " + r3)
-    print(" d. " + r4)
-    print()
-    reponse = input("Votre réponse : ")
+### Fonction qui permet de gérer les errreurs lors de la reponse de l'utlisateur
 
-    if reponse == choix_bonne_reponse:
+def demander_reponse_numerique_utilisateur(min, max):
+    reponse_str = input("Votre réponse entre " + str(min) + " et " + str(max) + " : ")
+    try:
+        reponse_int = int(reponse_str)
+        if min <= reponse_int <= max:
+            return reponse_int
+
+        print("ERREUR : Vous devez rentrer un nombre entre", min, "et", max)
+    except:
+        print("ERREUR : Veuillez rentrer un nombre")
+    ## fonction recursive si il y a des erreurs la fonction se rappel elle-même
+    return demander_reponse_numerique_utilisateur(min, max)
+
+############################################
+
+def poser_question(question):
+    titre_question = question[0]
+    choix = question[1]
+    choix_bonne_reponse = question[2]
+    print("Question: ")
+    print(" " + titre_question)
+
+    for i in range(len(choix)):
+        print(" " + str(i+1) + "- " + choix[i])
+
+    print()
+    resultat_response_correcte = False
+    # reponse_str = input("Votre réponse entre 1 et " + str(len(choix)) + " : ")
+    reponse_int = demander_reponse_numerique_utilisateur(1, len(choix))
+    if choix[reponse_int - 1].lower() == choix_bonne_reponse.lower():
         print("Bonne réponse")
-        score += 1
+        resultat_response_correcte = True
 
     else:
         print("Mauvaise réponse")
     print()
+    return resultat_response_correcte
 
+###############################
+### Fonction qui gère un questionnaire avec plusieurs questions
+
+def lancer_questionnaire(questionnaire):
+    score = 0
+    for question in questionnaire:
+        if poser_question(question):
+            score += 1
+    print("Score final :", score, "sur", len(questionnaire))
 
 ########################
 
-score = 0
-poser_question("Quelle est la capitale de la France?", "Paris", "Lyon", "Marseille", "Toulouse","a")
-poser_question("Quelle la capital de L'Italie ?", "Venise", "Naples", "Rome","Florence", "c")
+## Créer un tuple avec toute les questions
+questionnaire = (
+        ("Quelle est la capitale de la France ?", ("Paris", "Lyon", "Marseille", "Toulouse","Lille"), "Paris"),
+        ("Quelle est la capitale de l'Italie ?", ("Venise", "Naples", "Rome", "Florence"), "Rome"),
+        ("Quelle est la capitale de la Belgique ?", ("Bruxelles", "Liège", "Namur"), "Bruxelles")
+        )
 
 
-print("Score final :", score)
+
+lancer_questionnaire(questionnaire)
